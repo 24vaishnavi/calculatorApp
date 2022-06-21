6d77a5050e3bbcee2e7a831b42d578ecf1f3074f
unknown <you@example.com>
2022-06-21 09:49:06 +0530

    First Java Code

---
+++ Calculator.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int n1, n2;
+		int ans=1, c=0;
+		char choice;
+		
+		do
+		{
+			System.out.println("a.Addition\tb.Subtraction\tc.Multiplication\td.Division\te.Exit");
+			System.out.println("Enter your choice:");
+			choice = s.next().charAt(0);
+			
+			switch(choice)
+			{
+			case 'a':
+				System.out.println("Enter any two numbers: ");
+				n1 = s.nextInt();
+				n2 = s.nextInt();
+				c = n1 + n2;
+				System.out.println(n1+"+"+n2+"="+c);
+				break;
+				
+			case 'b':
+				System.out.println("Enter any two numbers: ");
+				n1 = s.nextInt();
+				n2 = s.nextInt();
+				if(n1>n2)
+				{
+					c = n1 - n2;
+					System.out.println(n1+"-"+n2+"="+c);
+				}
+				else
+				{
+					c = n2 - n1;
+					System.out.println(n2+"-"+n1+"="+c);
+				}
+				break;
+				
+			case 'c':
+				System.out.println("Enter any two numbers: ");
+				n1 = s.nextInt();
+				n2 = s.nextInt();
+				c = n1 * n2;
+				System.out.println(n1+"*"+n2+"="+c);
+				break;
+				
+			case 'd':
+				System.out.println("Enter any two numbers: ");
+				n1 = s.nextInt();
+				n2 = s.nextInt();
+				
+				if(n2==0)
+				{
+					System.out.println("Denominator should not be zero");
+				}
+				else
+				{
+					c = n1 / n2;
+					System.out.println(n1+"/"+n2+"="+c);
+				}
+				break;
+				
+			case 'e':
+				System.exit(0);
+				break;
+				
+			default:
+				System.out.println("WRONG CHOICE");
+			}
+			System.out.println("DO YOU WANT TO CONTINUE(1/0)?");
+			ans = s.nextInt();
+		}
+		while(ans!=0);
+	}
+}
